7320a067b6eb3a42b9c146f9a7a7f3b30c3f3841
Garretbre <67977807+Garretbre@users.noreply.github.com>
2021-01-06 15:34:18 -0800

    Projects
    Projects added

---
+++ AcmeAccountingSystems/AcmeAccountingSystems/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AcmeAccountingSystems
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems"+ Environment.NewLine + "Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
