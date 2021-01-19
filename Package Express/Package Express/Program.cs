3353e7904d79b321798b0c259a26366e847e1af6
Garretbre <67977807+Garretbre@users.noreply.github.com>
2021-01-19 13:23:31 -0800

    Update
    Corrections to step 185

--- Package Express/Package Express/Program.cs
+++ Package Express/Package Express/Program.cs
@@ -7,51 +7,68 @@ namespace Package_Express
         static void Main(string[] args)
         {
 
-            
+
 
             //Static written title
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
 
             //First question to User weight. weight greater than 50. display error message that reads. "Package too heavy to be shipped via Package Express. Have a good day". At this point the program end
             Console.WriteLine("Please enter the package weight:");
-            double currentWeight = Convert.ToInt32(Console.ReadLine());
+            int currentWeight = Convert.ToInt32(Console.ReadLine());
 
             if (currentWeight > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day");
-                Console.ReadLine();
             }
-             
+
+            //2nd
+            else
             
-            //Secound question to User width.
-            Console.WriteLine("Please enter the package width:");
-            double currentWidth = Convert.ToInt32(Console.ReadLine());
 
-
-            //Third question to User height.
-            Console.WriteLine("Please enter the package height:");
-            double currentHeight = Convert.ToInt32(Console.ReadLine());
-
-            //Fourth question to User length. length greater than 50. display error message that reads. "Package too big to be shipped via Package Express.". At this point the program would end
-            Console.WriteLine("Please enter the package length:");
-            double currentLength = Convert.ToInt32(Console.ReadLine());
-
-            if (currentLength > 50)
+                //Secound question to User width.
+                Console.WriteLine("Please enter the package width:");
+                int currentWidth = Convert.ToInt32(Console.ReadLine());
+            
+            if (currentWeight + currentWidth > 50)
             {
                 Console.WriteLine("Package too big to be shipped via Package Express.");
-                Console.ReadLine();
+            }
+            //3rd
+            else
+            
+                //Third question to User height.
+                Console.WriteLine("Please enter the package height:");
+                int currentHeight = Convert.ToInt32(Console.ReadLine());
+            
+            if (currentWeight + currentWidth + currentHeight > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+            }
+            //4th
+            else
+            
+                //Fourth question to User length. 
+                Console.WriteLine("Please enter the package length:");
+                int currentLength = Convert.ToInt32(Console.ReadLine());
+            
+            if (currentWeight + currentWidth + currentHeight + currentLength > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
             }
 
+            else
             
-            double result = currentWeight * currentWidth * currentHeight * currentLength;
+               Console.WriteLine($"Your estimated total for shipping this package is:    ${Convert.ToDecimal(currentWeight + currentWidth + currentHeight + currentLength) / 100}");
+
+                Console.WriteLine("Thank you!");
+
+                Console.ReadLine();
             
+                
+              
+                
 
-            Console.WriteLine($"Your estimated total for shipping this package is:    ${Convert.ToDecimal(result)/100}");
-            
 
-            Console.WriteLine("Thank you!");
-
-            Console.ReadLine();
         }
     }
 }
