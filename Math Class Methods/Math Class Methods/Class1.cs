dd523871a38dfc1ecab07918c24653467a55ba4a
Garretbre <67977807+Garretbre@users.noreply.github.com>
2021-02-12 11:44:21 -0800

    Math_Class_Method
    First commit

---
+++ Math Class Methods/Math Class Methods/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Math_Class_Methods;
+
+namespace Math_Class_Methods
+{
+    //1.Create a class. In that class, create three methods, each of which will take one integer parameter in and return an integer.
+    //The methods should do some math operation on the received parameter.Put this class in a separate.cs file in the application.
+    public class Class1
+    {
+        public Class1()
+       
+        {
+            string Add = oneNum + 5;
+            
+        }
+
+
+
+        public string Add { get; set; }
+        
+
+        //Method two
+
+
+
+        //Methid three
+
+
+
+
+    }
+}
---
+++ Math Class Methods/Math Class Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Math_Class_Methods;
+
+
+
+namespace Math_Class_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Math Operators");
+
+
+            //2. In the Main() program, ask the user what number they want to do the math operations on.
+            Console.WriteLine("Pick a number you would like to use math operations on ");
+            Console.Write("number: ");
+            int oneNum = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            //3. Call each method in turn, passing the user input to the method. Display the returned integer to the screen.
+            Console.WriteLine(Add);
+        }
+    }
+}
